b5bc8636613fc26e38c8a14e1179520e5b29ff2f
CaiFuLei007 <18871566937@163.com>
2026-05-25 17:56:38 +0800

    feat(model): add channel

---
+++ include/base/channel.h
@@ -0,0 +1,93 @@
+
+#pragma once
+
+/*
+    - 对所有套接字 进行管理
+    - 成员 : 
+            1) 文件描述符
+            2) 监听事件 , 就绪事件
+            3) 事件回调 : 读事件 , 写事件 , 错误事件,  关闭事件
+            4) eventloop 
+    - 接口 : 
+            1) 获取设置监听事件 , 就绪事件
+            2) 设置回调
+            3) 设置/关闭 读事件, 写事件监听
+
+*/
+
+
+#include <iostream>
+#include <functional>
+#include <unistd.h>
+
+class Channel
+{
+    using Callback = std::function<void()>;
+private:
+    int fd_;
+    int events_;
+    int revents_;
+    Callback read_callback_;
+    Callback write_callback_;
+    Callback error_callback_;
+    Callback event_callback_;
+
+public:
+    Channel(int fd)
+    :fd_(fd) , 
+    events_(0) ,
+    revents_(0)
+    {}
+
+    ~Channel()
+    {
+        close(fd_);
+    }
+
+    int GetEvents()
+    {
+        return events_;
+    }
+
+    void SetEvents(int events)
+    {   
+        events_ = events;
+    }
+
+    int GetRevents()
+    {
+        return revents_;
+    }
+
+    void SetRevents(int revents)
+    {
+        revents_ =revents;
+    }
+
+    void SetReadCallback(Callback cb)
+    {
+        read_callback_ = cb;
+    }
+
+    void SetWriteCallback(Callback cb)
+    {
+        write_callback_ = cb;
+    }
+
+    void SetErrorCallback(Callback cb)
+    {
+        error_callback_ = cb;
+    }
+
+    void SetEventCallback(Callback cb)
+    {
+        event_callback_ = cb;
+    }
+
+    void Handle();   // 处理就绪事件
+
+    void ReadAble();
+    void WriteAble();
+    void UnReadAble();
+    void UnWriteAble();
+};
